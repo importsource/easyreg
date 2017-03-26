b2eb5d4f5eaf89a1c24b8ebdc40906f099f5baee
Charles <hezhuofan24@126.com>
2017-03-26 21:12:15 +0800

    Initial commit

---
+++ Demo.java
@@ -0,0 +1,24 @@
+/**
+ * @author hezhuofan
+ */
+public class Demo {
+    public static void main(String[] args) {
+        VerbalExpression testRegex = VerbalExpression.regex()
+                .startOfLine().then("http").maybe("s")
+                .then("://")
+                .maybe("www.").anythingBut(" ")
+                .then(".com")
+                .endOfLine()
+                .build();
+
+        // Create an example URL
+        String url = "https://www.google.com";
+
+        // Use VerbalExpression's testExact() method to test if the entire string matches the regex
+        boolean is = testRegex.testExact(url); //True
+        System.out.println(is);
+
+        System.out.println(testRegex.toString()); // Outputs the regex used:
+        // ^(?:http)(?:s)?(?:\:\/\/)(?:www\.)?(?:[^\ ]*)$
+    }
+}
